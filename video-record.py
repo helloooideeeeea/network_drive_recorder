16443d4eb91c55c8235c89ce2adccd755f538f44
Yasushi Sakita <yasushi.sakita@gmail.com>
2023-01-07 19:21:57 +0900

    バグ修正

--- video-record.py
+++ video-record.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     mediaLocation = os.path.abspath(CURRENT_DIR + "/" + "segment%05d.ts")
 
     # TODO LEDで青色にする
-    ret = subprocess.call(
-        f"gst-launch-1.0 libcamerasrc ! videoconvert ! videoscale ! clockoverlay time-format='%D %H:%M:%S' ! v4l2h264enc ! 'video/x-h264,level=(string)4' ! h264parse ! mpegtsmux name=mux ! hlssink max-files=0 target-duration=10 location={mediaLocation} playlist-location={playlistLocation} sync=false")
+    ret = subprocess.run(
+        f"/usr/bin/gst-launch-1.0 libcamerasrc ! videoconvert ! videoscale ! clockoverlay time-format='%D %H:%M:%S' ! v4l2h264enc ! 'video/x-h264,level=(string)4' ! h264parse ! mpegtsmux name=mux ! hlssink max-files=0 target-duration=10 location={mediaLocation} playlist-location={playlistLocation} sync=false", shell=True)
 
     # TODO LEDで赤色にして、さらにブザーを鳴らす
